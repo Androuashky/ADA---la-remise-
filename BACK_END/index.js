879097e790ec0c15b5e46a1843fecccefd0d08a2
jottavy <serveurs_dessin.7i@icloud.com>
2026-09-07 15:02:03 +0200

    Add db.js & index.js

---
+++ BACK_END/index.js
@@ -0,0 +1,37 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" with { type: "json" };
+import express from "express";
+import cors from "cors"
+import objetsRouter from './routes/objets.js';
+import categoriesRouter from './routes/categories.js';
+import depotsRouter from './routes/depots.js';
+import personnesRouter from './routes/personnes.js';
+import statsRouter from './routes/stats.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/objets', objetsRouter);
+app.use('/api/categories', categoriesRouter);
+app.use('/api/depots', depotsRouter);
+app.use('/api/personnes', personnesRouter);
+app.use('/api/stats', statsRouter);
+
+app.use((err, req, res, next) => {
+  console.error("Erreur centralisée :", err.stack); // pour debugger : renvoie une chaîne de caractères qui décrit le chemin d'exécution depuis le point où l'erreur a été levée jusqu'à sa capture
+
+  if (err.code === "23503") {
+    return res.status(404).json({ error: "Ressource liée introuvable (clé étrangère inexistante)." });
+  }
+
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({ error: err.message || "Erreur interne du serveur" });
+});
+
+app.listen(3000, () => {
+  console.log(`🚀 Serveur express démarré sur http://localhost:3000`);
+});
