7063d43cfecffd0b277bf3bfcb03f4f2de6f66d8
Andréa Vignon <vignon.andrea@gmail.com>
2026-09-08 16:17:42 +0200

    modification du .env / applisten et suppression des routes

--- BACK_END/index.js
+++ BACK_END/index.js
@@ -2,11 +2,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json" };
 import express from "express";
 import cors from "cors";
-import objetsRouter from './routes/objets.js';
-import categoriesRouter from './routes/categories.js';
-import depotsRouter from './routes/depots.js';
-import personnesRouter from './routes/personnes.js';
-import statsRouter from './routes/stats.js';
+
 
 const app = express();
 
@@ -15,11 +11,6 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/api/objets', objetsRouter);
-app.use('/api/categories', categoriesRouter);
-app.use('/api/depots', depotsRouter);
-app.use('/api/personnes', personnesRouter);
-app.use('/api/stats', statsRouter);
 
 app.use((err, req, res, next) => {
   console.error("Erreur centralisée :", err.stack); // pour debugger : renvoie une chaîne de caractères qui décrit le chemin d'exécution depuis le point où l'erreur a été levée jusqu'à sa capture
@@ -32,6 +23,5 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ error: err.message || "Erreur interne du serveur" });
 });
 
-app.listen(3000, () => {
-  console.log(`🚀 Serveur express démarré sur http://localhost:3000`);
-});
+app.listen(process.env.PORT);
+
